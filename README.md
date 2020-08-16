a33168fcaf3bd2b94507c183bfa2aef970dde1fb
Kishan Dalwadi <dalwadikishan456@gmail.com>
2020-08-16 19:33:28 +0530

    Added steps in README.md

--- README.md
+++ README.md
@@ -1,24 +1,20 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Rails version - Rails 5.2.4.3
 
-Things you may want to cover:
+Ruby version - ruby 2.5.1p57
 
-* Ruby version
+rails db:create
 
-* System dependencies
+rails db:migrate
 
-* Configuration
+rails s
 
-* Database creation
+start redis
 
-* Database initialization
+start sidekiq
 
-* How to run the test suite
+Create doctors account
+Create patients account
 
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+Note: If you want to send email then you have to config it from "development.rb" or "production.rb".
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_08_16_132128) do
 
-  create_table "appointments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "appointments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "doctor_id"
     t.string "patient_id"
     t.datetime "created_at", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_16_132128) do
     t.string "accepted_by"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
